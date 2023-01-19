80d62fe549d8fccbf895d55307840295a637586e
Mayank Mangukiya <godhani.jaydip145@gmail.com>
2023-01-19 17:54:01 +0530

    Feature: Rating bar function
    Rating bar implement to give rate app

--- bhagavad_gita/lib/screens/home_screen.dart/about_gita_page.dart
+++ bhagavad_gita/lib/screens/home_screen.dart/about_gita_page.dart
@@ -3,6 +3,8 @@ import 'package:bhagavad_gita/Constant/app_size_config.dart';
 import 'package:bhagavad_gita/localization/demo_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:rating_dialog/rating_dialog.dart';
+import 'package:store_redirect/store_redirect.dart';
 
 class AboutGitaScreen extends StatefulWidget {
   @override
@@ -11,198 +13,288 @@ class AboutGitaScreen extends StatefulWidget {
 
 class _AboutGitaScreenState extends State<AboutGitaScreen>
     with AutomaticKeepAliveClientMixin {
+  // final Uri _url = Uri.parse(
+  //     'https://bhagavadgita.io/privacy-policy/');
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 2.7,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/image_aboutGita.jpg'),
-                        fit: BoxFit.cover),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 2.7,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    image: DecorationImage(
-                        image: AssetImage(
-                            'assets/images/black_imageAboutGita.png'),
-                        fit: BoxFit.cover),
-                  ),
-                ),
-                Positioned(
-                  top: MediaQuery.of(context).size.height / kPadding * 2.7,
-                  left: kDefaultPadding,
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          DemoLocalization.of(context)!
-                              .getTranslatedValue('about_gita')
-                              .toString(),
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline1!
-                              .copyWith(fontSize: 30, color: whiteColor),
-                        ),
-                        // Text(
-                        //   DemoLocalization.of(context)!
-                        //       .getTranslatedValue('gita')
-                        //       .toString(),
-                        //   textAlign: TextAlign.justify,
-                        //   style: Theme.of(context)
-                        //       .textTheme
-                        //       .headline1!
-                        //       .copyWith(fontSize: 28, color: orangeColor),
-                        // ),
-                      ],
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 2.7,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                          image:
+                              AssetImage('assets/images/image_aboutGita.jpg'),
+                          fit: BoxFit.cover),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Stack(
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(kDefaultPadding * 2),
-                          child: SvgPicture.asset(
-                            'assets/icons/icon_quote_AboutGita.svg',
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 2.7,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/black_imageAboutGita.png'),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  Positioned(
+                    top: MediaQuery.of(context).size.height / kPadding * 2.7,
+                    left: kDefaultPadding,
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            DemoLocalization.of(context)!
+                                .getTranslatedValue('about_gita')
+                                .toString(),
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline1!
+                                .copyWith(fontSize: 30, color: whiteColor),
                           ),
-                        ),
-                      ],
-                    ),
-                    Positioned(
-                      top: kDefaultPadding * 2.7,
-                      left: 0,
-                      right: 0,
-                      child: Container(
-                        child: Text(
-                          DemoLocalization.of(context)!
-                              .getTranslatedValue('unlikeModernWriting')
-                              .toString(),
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline1!
-                              .copyWith(fontSize:
-                          (Localizations.localeOf(context).languageCode == 'hi') ?
-                          21 : 20, color: orangeColor),
-                        ),
+                          // Text(
+                          //   DemoLocalization.of(context)!
+                          //       .getTranslatedValue('gita')
+                          //       .toString(),
+                          //   textAlign: TextAlign.justify,
+                          //   style: Theme.of(context)
+                          //       .textTheme
+                          //       .headline1!
+                          //       .copyWith(fontSize: 28, color: orangeColor),
+                          // ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              child: Column(
+                  ),
+                ],
+              ),
+              Column(
                 children: [
-                  RichText(
-                    textAlign: TextAlign.left,
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
+                  Stack(
+                    children: [
+                      Row(
+                        // mainAxisAlignment: MainAxisAlignment.start,
+                        // crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding:
+                                EdgeInsets.only(top: 20, left: 20, bottom: 70),
+                            child: SvgPicture.asset(
+                              'assets/icons/icon_quote_AboutGita.svg',
+                              height: 30,
+                              width: 30,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Positioned(
+                        top: kDefaultPadding * 2.7,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          child: Text(
+                            DemoLocalization.of(context)!
+                                .getTranslatedValue('unlikeModernWriting')
+                                .toString(),
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline1!
+                                .copyWith(
+                                    fontSize: (Localizations.localeOf(context)
+                                                .languageCode ==
+                                            'hi')
+                                        ? 21
+                                        : 20,
+                                    color: orangeColor),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Column(
+                  children: [
+                    RichText(
+                      textAlign: TextAlign.left,
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
                             text: DemoLocalization.of(context)!
                                 .getTranslatedValue('gitaStory')
                                 .toString(),
                             style: Theme.of(context)
-                                .textTheme.subtitle1!
-                                .copyWith(fontSize: (Localizations.localeOf(context).languageCode == 'hi') ?
-                            20 : 18, color: blackColor),
-                        )
+                                .textTheme
+                                .subtitle1!
+                                .copyWith(
+                                    fontSize: (Localizations.localeOf(context)
+                                                .languageCode ==
+                                            'hi')
+                                        ? 20
+                                        : 18,
+                                    color: blackColor),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: kDefaultPadding * 1.5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                            "assets/icons/icon_left_rtansection.svg"),
+                        SizedBox(width: kDefaultPadding),
+                        Text(
+                          DemoLocalization.of(context)!
+                              .getTranslatedValue('story')
+                              .toString(),
+                          style: Theme.of(context)
+                              .textTheme
+                              .subtitle1!
+                              .copyWith(
+                                  fontSize: (Localizations.localeOf(context)
+                                              .languageCode ==
+                                          'hi')
+                                      ? 20
+                                      : 18,
+                                  fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(width: kDefaultPadding),
+                        SvgPicture.asset(
+                            "assets/icons/icon_right_translation.svg")
                       ],
                     ),
-                  ),
-                  SizedBox(height: kDefaultPadding * 1.5),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                          "assets/icons/icon_left_rtansection.svg"),
-                      SizedBox(width: kDefaultPadding),
-                      Text(
+                    SizedBox(height: kDefaultPadding),
+                    Text(
                         DemoLocalization.of(context)!
-                            .getTranslatedValue('story')
+                            .getTranslatedValue('gitaStoryDetail')
                             .toString(),
                         style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                            fontSize: (Localizations.localeOf(context).languageCode == 'hi') ?
-                            20 : 18, fontWeight: FontWeight.w600),
-                      ),
-                      SizedBox(width: kDefaultPadding),
-                      SvgPicture.asset(
-                          "assets/icons/icon_right_translation.svg")
-                    ],
-                  ),
-                  SizedBox(height: kDefaultPadding),
-                  Text(
+                            fontSize:
+                                (Localizations.localeOf(context).languageCode ==
+                                        'hi')
+                                    ? 20
+                                    : 18,
+                            color: blackColor)),
+                    SizedBox(height: kDefaultPadding * 1.5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                            "assets/icons/icon_left_rtansection.svg"),
+                        SizedBox(width: kDefaultPadding),
+                        Text(
+                          DemoLocalization.of(context)!
+                              .getTranslatedValue('conclusion')
+                              .toString(),
+                          style: Theme.of(context)
+                              .textTheme
+                              .subtitle1!
+                              .copyWith(
+                                  fontSize: (Localizations.localeOf(context)
+                                              .languageCode ==
+                                          'hi')
+                                      ? 20
+                                      : 18,
+                                  fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(width: kDefaultPadding),
+                        SvgPicture.asset(
+                            "assets/icons/icon_right_translation.svg")
+                      ],
+                    ),
+                    SizedBox(height: kDefaultPadding),
+                    Text(
                       DemoLocalization.of(context)!
-                          .getTranslatedValue('gitaStoryDetail')
+                          .getTranslatedValue('conclusionDetail')
                           .toString(),
-                      style: Theme.of(context)
-                          .textTheme.subtitle1!
-                          .copyWith(fontSize: (Localizations.localeOf(context).languageCode == 'hi') ?
-                      20 : 18, color: blackColor)),
-                  SizedBox(height: kDefaultPadding * 1.5),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                          "assets/icons/icon_left_rtansection.svg"),
-                      SizedBox(width: kDefaultPadding),
-                      Text(
-                        DemoLocalization.of(context)!
-                            .getTranslatedValue('conclusion')
-                            .toString(),
-                        style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                            fontSize: (Localizations.localeOf(context).languageCode == 'hi') ?
-                            20 : 18, fontWeight: FontWeight.w600),
-                      ),
-                      SizedBox(width: kDefaultPadding),
-                      SvgPicture.asset(
-                          "assets/icons/icon_right_translation.svg")
-                    ],
-                  ),
-                  SizedBox(height: kDefaultPadding),
-                  Text(
-                    DemoLocalization.of(context)!
-                        .getTranslatedValue('conclusionDetail')
-                        .toString(),
-                      style: Theme.of(context)
-                          .textTheme.subtitle1!
-                          .copyWith(fontSize: (Localizations.localeOf(context).languageCode == 'hi') ?
-                      20 : 18, color: blackColor),
-                  ),
-                  SizedBox(height: kDefaultPadding),
-                ],
+                      style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                          fontSize:
+                              (Localizations.localeOf(context).languageCode ==
+                                      'hi')
+                                  ? 20
+                                  : 18,
+                          color: blackColor),
+                    ),
+                    SizedBox(height: kDefaultPadding),
+                  ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: kDefaultPadding * 1.5),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.small(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => _dialog,
+            );
+          },
+          child: Icon(
+            Icons.star,
+            color: Color(0xFFFFFFFF),
+          ),
+          backgroundColor: Color(0xFF000000),
         ),
       ),
     );
   }
+
+  final _dialog = RatingDialog(
+    starSize: 35,
+    // your app's name?
+    title: Text(
+      'Rate Us On play Store',
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+    ),
+    // encourage your user to leave a high rating?
+    message: Text(
+      'Tap a star to set your rating',
+      style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
+    ),
+    // your app's logo?
+    image: Image.asset(
+      "assets/images/splashScreenLogo.JPG",
+      height: 100,
+      // width: 50,
+    ),
+    submitButtonText: "Submit",
+    // onCancelled: () => print('cancelled'),
+    onSubmitted: (response) {
+      print('rating: ${response.rating}, comment: ${response.comment}');
+      // TODO: add your own logic
+      if (response.rating < 3.0) {
+        // send their comments to your email or anywhere you wish
+        // ask the user to contact you instead of leaving a bad review
+      } else {
+        //go to app store
+        StoreRedirect.redirect(
+            androidAppId: 'com.gitainitiative.bhagavadgita',
+            iOSAppId: 'com.gitainitiative.bhagavadgita');
+      }
+    },
+  );
 
   @override
   bool get wantKeepAlive => true;
