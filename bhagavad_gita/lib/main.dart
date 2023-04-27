fab8b4d8a7181289e56631b0f012f1c914bdb638
jaydip godhani <godhani.jaydip145@gmail.com>
2023-04-27 17:36:16 +0530

    Bhagavad Gita
    - breaking word issue

--- bhagavad_gita/lib/main.dart
+++ bhagavad_gita/lib/main.dart
@@ -16,7 +16,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 ///// firbase push notification in Background
